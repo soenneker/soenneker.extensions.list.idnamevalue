aa48f74b9d4223a2ad1fc43ebc8f3a57362d4371
Jake Soenneker <jake@soenneker.com>
2024-11-08 15:07:06 -0600

    Fleshed out

--- src/ListIdNamePairExtension.cs
+++
@@ -1,8 +0,0 @@
-namespace Soenneker.Extensions.List.IdNameValue;
-
-/// <summary>
-/// A collection of helpful List{IdNameValue} extension methods
-/// </summary>
-public static class ListIdNamePairExtension
-{
-}
