4294b692001bea1209b571adbd8983c495de43d2
Jake Soenneker <jake@soeneker.com>
2024-12-20 17:34:21 +0000

    Tests

---
+++ test/Soenneker.Extensions.List.IdNameValue.Tests/ListIdNameValueExtensionTests.cs
@@ -0,0 +1,13 @@
+using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Extensions.List.IdNameValue.Tests;
+
+public class ListIdNameValueExtensionTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
