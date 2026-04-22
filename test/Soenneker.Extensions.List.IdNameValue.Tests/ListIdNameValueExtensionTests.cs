8ef268c942ab7c833625526a5c202b0b5ae2815d
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:02:00 -0500

    TUnit conversion

--- test/Soenneker.Extensions.List.IdNameValue.Tests/ListIdNameValueExtensionTests.cs
+++ test/Soenneker.Extensions.List.IdNameValue.Tests/ListIdNameValueExtensionTests.cs
@@ -1,11 +1,10 @@
 using Soenneker.Tests.Unit;
-using Xunit;
 
 namespace Soenneker.Extensions.List.IdNameValue.Tests;
 
 public class ListIdNameValueExtensionTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
